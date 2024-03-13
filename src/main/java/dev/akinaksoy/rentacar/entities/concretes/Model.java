fe9c32054a88157b8461c6d080bb888b0e94b157
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-13 11:38:53 +0300

    refactor Model.java

--- src/main/java/dev/akinaksoy/rentacar/entities/concretes/Model.java
+++ src/main/java/dev/akinaksoy/rentacar/entities/concretes/Model.java
@@ -1,12 +1,12 @@
 package dev.akinaksoy.rentacar.entities.concretes;
 
 import dev.akinaksoy.rentacar.core.entities.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +17,21 @@ public class Model extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "brandId")
+    private Brand brand;
+
+    @ManyToOne
+    @JoinColumn(name = "fuelId")
+    private Fuel fuel;
+
+    @ManyToOne
+    @JoinColumn(name = "transmissionId")
+    private Transmission transmission;
+
+    @OneToMany(mappedBy = "model")
+    private List<Car> cars;
 
 
 
