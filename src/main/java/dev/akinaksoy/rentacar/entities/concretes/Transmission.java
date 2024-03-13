01f103cd0b8136b81709a6b3ce86ead334d4c396
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-13 11:38:36 +0300

    refactor Transmission.java

--- src/main/java/dev/akinaksoy/rentacar/entities/concretes/Transmission.java
+++ src/main/java/dev/akinaksoy/rentacar/entities/concretes/Transmission.java
@@ -3,10 +3,13 @@ package dev.akinaksoy.rentacar.entities.concretes;
 import dev.akinaksoy.rentacar.core.entities.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,4 +20,7 @@ public class Transmission extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(mappedBy = "transmission")
+    private List<Model> models;
 }
