a75b374563caf91e40e54ead6b1653e640276d8c
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-16 02:35:23 +0300

    CREATE -> CreatedFuelResponse

---
+++ src/main/java/dev/akinaksoy/rentacar/business/dtos/responses/fuel/CreatedFuelResponse.java
@@ -0,0 +1,13 @@
+package dev.akinaksoy.rentacar.business.dtos.responses.fuel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CreatedFuelResponse {
+    private  int id;
+    private String name;
+}
