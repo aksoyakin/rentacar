3cd817e11976715face4d840b84a86726d0d7442
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-15 11:06:02 +0300

    CREATE -> GetAllBrandResponse

---
+++ src/main/java/dev/akinaksoy/rentacar/business/dtos/responses/GetAllBrandResponse.java
@@ -0,0 +1,15 @@
+package dev.akinaksoy.rentacar.business.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GetAllBrandResponse {
+    private int id;
+    private String name;
+}
