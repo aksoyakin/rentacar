7c2e166145aef2e3666c1fe54919a8e026b9d159
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-15 11:09:09 +0300

    REFACTOR -> RentacarApplication

--- src/main/java/dev/akinaksoy/rentacar/RentacarApplication.java
+++ src/main/java/dev/akinaksoy/rentacar/RentacarApplication.java
@@ -1,7 +1,9 @@
 package dev.akinaksoy.rentacar;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RentacarApplication {
@@ -9,5 +11,11 @@ public class RentacarApplication {
     public static void main(String[] args) {
         SpringApplication.run(RentacarApplication.class, args);
     }
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+
 
 }
