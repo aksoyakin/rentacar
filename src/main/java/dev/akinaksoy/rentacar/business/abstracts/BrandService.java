2a4d4b2b305158dde6978a1251e975099d656c42
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-15 11:06:18 +0300

    CREATE -> BrandService

--- src/main/java/dev/akinaksoy/rentacar/business/abstracts/BrandService.java
+++ src/main/java/dev/akinaksoy/rentacar/business/abstracts/BrandService.java
@@ -1,4 +1,17 @@
 package dev.akinaksoy.rentacar.business.abstracts;
 
+
+import dev.akinaksoy.rentacar.business.dtos.requests.CreateBrandRequest;
+import dev.akinaksoy.rentacar.business.dtos.responses.CreatedBrandResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.GetAllBrandResponse;
+
+import java.util.List;
+
 public interface BrandService {
+    CreatedBrandResponse createBrand(
+            CreateBrandRequest request
+    );
+    List<GetAllBrandResponse> getAllBrands(
+
+    );
 }
