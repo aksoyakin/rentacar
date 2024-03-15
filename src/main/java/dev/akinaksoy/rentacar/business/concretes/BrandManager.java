b562ef479d585e162384f7689d2851e8f2c2efff
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-15 11:05:02 +0300

    CREATE -> ModelMapperManager.java

--- src/main/java/dev/akinaksoy/rentacar/business/concretes/BrandManager.java
+++ src/main/java/dev/akinaksoy/rentacar/business/concretes/BrandManager.java
@@ -1,5 +1,73 @@
 package dev.akinaksoy.rentacar.business.concretes;
 
-public class BrandManager {
+import dev.akinaksoy.rentacar.business.abstracts.BrandService;
+import dev.akinaksoy.rentacar.business.dtos.requests.CreateBrandRequest;
+import dev.akinaksoy.rentacar.business.dtos.responses.CreatedBrandResponse;
 
+import dev.akinaksoy.rentacar.business.dtos.responses.GetAllBrandResponse;
+import dev.akinaksoy.rentacar.core.utilities.mapping.ModelMapperService;
+import dev.akinaksoy.rentacar.dataaccess.abstracts.BrandRepository;
+import dev.akinaksoy.rentacar.entities.concretes.Brand;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class BrandManager implements BrandService {
+
+    private BrandRepository brandRepository;
+    private ModelMapperService modelMapperService;
+
+
+    @Override
+    public CreatedBrandResponse createBrand(
+            CreateBrandRequest request
+    ) {
+        Brand brand = this.modelMapperService
+                .forRequest()
+                .map(request,Brand.class);
+
+        brand.setCreatedDate(LocalDateTime.now());
+
+        Brand createdBrand = this.brandRepository.save(brand);
+
+        CreatedBrandResponse response = this.modelMapperService
+                .forResponse().map(brand, CreatedBrandResponse.class);
+
+        return response;
+
+
+
+        /*
+        MANUEL MAPPING!
+
+        Brand brand = new Brand();
+        brand.setName(request.getName());
+        brand.setCreatedDate(LocalDateTime.now());
+        this.brandRepository.save(brand);
+
+        return brand;
+         */
+    }
+
+    @Override
+    public List<GetAllBrandResponse> getAllBrands(
+
+    ) {
+        List<Brand> brands = brandRepository.findAll();
+
+        List<GetAllBrandResponse> brandResponses =
+                brands.stream().map(brand -> modelMapperService
+                                .forResponse()
+                                .map(brand, GetAllBrandResponse.class))
+                                .collect(Collectors.toList());
+
+        return brandResponses;
+    }
 }
