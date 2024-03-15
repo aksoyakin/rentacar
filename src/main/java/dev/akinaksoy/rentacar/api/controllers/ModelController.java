14c66a6cc332085a43148d2291a73488efc6da00
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-16 02:28:10 +0300

    CREATE -> ModelController

---
+++ src/main/java/dev/akinaksoy/rentacar/api/controllers/ModelController.java
@@ -0,0 +1,35 @@
+package dev.akinaksoy.rentacar.api.controllers;
+
+import dev.akinaksoy.rentacar.business.abstracts.ModelService;
+import dev.akinaksoy.rentacar.business.dtos.requests.model.CreateModelRequest;
+import dev.akinaksoy.rentacar.business.dtos.responses.model.CreatedModelResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.model.GetAllModelResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/models")
+public class ModelController {
+    private ModelService modelService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreatedModelResponse createModel(
+            @Valid @RequestBody CreateModelRequest request
+    ){
+        return modelService.createModel(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<GetAllModelResponse> getAllModels(
+
+    ){
+        return modelService.getAllModels();
+    }
+}
