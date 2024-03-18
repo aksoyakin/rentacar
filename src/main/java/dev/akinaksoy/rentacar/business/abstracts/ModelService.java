3985eff5ac17b27af5c9ab56cb5a5de326c16fae
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:19:43 +0300

    UPDATE -> ModelService

--- src/main/java/dev/akinaksoy/rentacar/business/abstracts/ModelService.java
+++ src/main/java/dev/akinaksoy/rentacar/business/abstracts/ModelService.java
@@ -1,9 +1,11 @@
 package dev.akinaksoy.rentacar.business.abstracts;
 
 import dev.akinaksoy.rentacar.business.dtos.requests.model.CreateModelRequest;
+import dev.akinaksoy.rentacar.business.dtos.requests.model.UpdateModelRequest;
 import dev.akinaksoy.rentacar.business.dtos.responses.model.CreatedModelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.model.GetAllModelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.model.GetModelByIdResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.model.UpdateModelResponse;
 
 import java.util.List;
 
@@ -15,6 +17,10 @@ public interface ModelService {
 
     );
     GetModelByIdResponse getModelById(
+            int id
+    );
+    UpdateModelResponse updateModelById(
+            UpdateModelRequest request,
             int id
     );
 }
