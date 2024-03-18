8776ae8ea7ec6f63a9ef345e68de5be64be9f4a2
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:15:09 +0300

    UPDATE -> CarRepository

--- src/main/java/dev/akinaksoy/rentacar/dataaccess/abstracts/CarRepository.java
+++ src/main/java/dev/akinaksoy/rentacar/dataaccess/abstracts/CarRepository.java
@@ -3,5 +3,9 @@ package dev.akinaksoy.rentacar.dataaccess.abstracts;
 import dev.akinaksoy.rentacar.entities.concretes.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CarRepository extends JpaRepository<Car,Integer> {
+    boolean existsByPlate(String plate);
+
 }
