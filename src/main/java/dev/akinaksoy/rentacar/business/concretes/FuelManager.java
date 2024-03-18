a68c39fd2951965490c0981e2175045b42f8aa22
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:22:04 +0300

    UPDATE -> Update method added to the FuelManager

--- src/main/java/dev/akinaksoy/rentacar/business/concretes/FuelManager.java
+++ src/main/java/dev/akinaksoy/rentacar/business/concretes/FuelManager.java
@@ -2,9 +2,11 @@ package dev.akinaksoy.rentacar.business.concretes;
 
 import dev.akinaksoy.rentacar.business.abstracts.FuelService;
 import dev.akinaksoy.rentacar.business.dtos.requests.fuel.CreateFuelRequest;
+import dev.akinaksoy.rentacar.business.dtos.requests.fuel.UpdateFuelRequest;
 import dev.akinaksoy.rentacar.business.dtos.responses.fuel.CreatedFuelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.fuel.GetAllFuelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.fuel.GetFuelByIdResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.fuel.UpdateFuelResponse;
 import dev.akinaksoy.rentacar.core.utilities.mapping.ModelMapperService;
 import dev.akinaksoy.rentacar.dataaccess.abstracts.FuelRepository;
 import dev.akinaksoy.rentacar.entities.concretes.Fuel;
@@ -58,10 +60,34 @@ public class FuelManager implements FuelService {
             int id
     ) {
         Fuel fuel = fuelRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("There is no fuel for this ID"));
+                .orElseThrow(() -> new RuntimeException("There is no fuel for this ID."));
 
         GetFuelByIdResponse response = modelMapperService.forResponse()
                 .map(fuel,GetFuelByIdResponse.class);
+
+        return response;
+    }
+
+    @Override
+    public UpdateFuelResponse updateFuelById(
+            UpdateFuelRequest request,
+            int id
+    ) {
+        Fuel fuel = fuelRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("There is no fuel for this ID."));
+
+        Fuel updatedFuel = modelMapperService.forRequest()
+                .map(request,Fuel.class);
+
+        fuel.setId(id);
+        fuel.setUpdatedDate(LocalDateTime.now());
+
+        fuel.setName(updatedFuel.getName());
+
+        fuelRepository.save(fuel);
+
+        UpdateFuelResponse response = modelMapperService.forResponse()
+                .map(fuel,UpdateFuelResponse.class);
 
         return response;
     }
