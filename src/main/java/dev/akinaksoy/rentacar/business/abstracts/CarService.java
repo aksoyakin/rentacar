ddfaa8e954f336dedc68d6c174650d3b06f584a8
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:19:15 +0300

    UPDATE -> CarService

--- src/main/java/dev/akinaksoy/rentacar/business/abstracts/CarService.java
+++ src/main/java/dev/akinaksoy/rentacar/business/abstracts/CarService.java
@@ -3,9 +3,11 @@ package dev.akinaksoy.rentacar.business.abstracts;
 
 
 import dev.akinaksoy.rentacar.business.dtos.requests.car.CreateCarRequest;
+import dev.akinaksoy.rentacar.business.dtos.requests.car.UpdateCarRequest;
 import dev.akinaksoy.rentacar.business.dtos.responses.car.CreatedCarResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.car.GetAllCarResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.car.GetCarByIdResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.car.UpdateCarResponse;
 
 import java.util.List;
 
@@ -17,6 +19,10 @@ public interface CarService {
 
     );
     GetCarByIdResponse getCarById(
+            int id
+    );
+    UpdateCarResponse updateCarById(
+            UpdateCarRequest request,
             int id
     );
 }
