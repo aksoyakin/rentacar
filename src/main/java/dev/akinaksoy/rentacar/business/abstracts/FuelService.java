c8cc51d9271ddbfb4c414cd570dc34586d271751
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:19:30 +0300

    UPDATE -> FuelService

--- src/main/java/dev/akinaksoy/rentacar/business/abstracts/FuelService.java
+++ src/main/java/dev/akinaksoy/rentacar/business/abstracts/FuelService.java
@@ -1,9 +1,11 @@
 package dev.akinaksoy.rentacar.business.abstracts;
 
 import dev.akinaksoy.rentacar.business.dtos.requests.fuel.CreateFuelRequest;
+import dev.akinaksoy.rentacar.business.dtos.requests.fuel.UpdateFuelRequest;
 import dev.akinaksoy.rentacar.business.dtos.responses.fuel.CreatedFuelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.fuel.GetAllFuelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.fuel.GetFuelByIdResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.fuel.UpdateFuelResponse;
 
 import java.util.List;
 
@@ -15,6 +17,10 @@ public interface FuelService {
 
     );
     GetFuelByIdResponse getFuelById(
+            int id
+    );
+    UpdateFuelResponse updateFuelById(
+            UpdateFuelRequest request,
             int id
     );
 
