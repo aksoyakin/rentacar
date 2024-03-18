ed45ed647e83cef02b8f1434ef4cf1da1526cfac
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:20:08 +0300

    UPDATE -> TransmissionService

--- src/main/java/dev/akinaksoy/rentacar/business/abstracts/TransmissionService.java
+++ src/main/java/dev/akinaksoy/rentacar/business/abstracts/TransmissionService.java
@@ -1,10 +1,12 @@
 package dev.akinaksoy.rentacar.business.abstracts;
 
 import dev.akinaksoy.rentacar.business.dtos.requests.transmission.CreateTransmissionRequest;
+import dev.akinaksoy.rentacar.business.dtos.requests.transmission.UpdateTransmissionRequest;
 import dev.akinaksoy.rentacar.business.dtos.responses.model.CreatedModelResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.transmission.CreatedTransmissionResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.transmission.GetAllTransmissionResponse;
 import dev.akinaksoy.rentacar.business.dtos.responses.transmission.GetTransmissionByIdResponse;
+import dev.akinaksoy.rentacar.business.dtos.responses.transmission.UpdateTransmissionResponse;
 
 import java.util.List;
 
@@ -18,6 +20,10 @@ public interface TransmissionService {
 
     );
     GetTransmissionByIdResponse getTransmissionById(
+            int id
+    );
+    UpdateTransmissionResponse updateTransmissionById(
+            UpdateTransmissionRequest request,
             int id
     );
 
