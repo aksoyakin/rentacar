1ff06d69c832f2172852164c27f59b70039b6948
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:15:23 +0300

    UPDATE -> ModelRepository

--- src/main/java/dev/akinaksoy/rentacar/dataaccess/abstracts/ModelRepository.java
+++ src/main/java/dev/akinaksoy/rentacar/dataaccess/abstracts/ModelRepository.java
@@ -3,5 +3,9 @@ package dev.akinaksoy.rentacar.dataaccess.abstracts;
 import dev.akinaksoy.rentacar.entities.concretes.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ModelRepository extends JpaRepository<Model,Integer> {
+    Optional<Model> findByNameIgnoreCase(String name);
+
 }
