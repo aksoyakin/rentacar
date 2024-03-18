26c41d008524510b9ca25594cdb12f09fb9e7783
AKINAKSOY <akinaksoyiletisim@gmail.com>
2024-03-18 14:12:48 +0300

    CREATE -> BusinessException

---
+++ src/main/java/dev/akinaksoy/rentacar/core/utilities/exceptions/types/BusinessException.java
@@ -0,0 +1,9 @@
+package dev.akinaksoy.rentacar.core.utilities.exceptions.types;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(
+            String message
+    ) {
+        super(message);
+    }
+}
